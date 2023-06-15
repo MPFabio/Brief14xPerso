3df2e88d490d0c3d8eab0435ba9c435931939335
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-15 14:48:27 +0200

    Update init-script.sh

--- azure-webserver/init-script.sh
+++ azure-webserver/init-script.sh
@@ -4,10 +4,5 @@ sudo apt install curl
 #Installing Docker
 sudo curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
-#Creating container
-cd app
-sudo docker build -t fabio-brief14 
-sudo docker tag fabio-tp-game fabiomp/fabio-brief14
-sudo docker login -u fabiomp -p Aucunmdp69
-sudo docker push fabiomp/fabio-brief14           
-sudo docker run -d -p 5005:1234 --name docker-nginx fabiomp/docker-nginx:latest
+#Creating container        
+sudo docker run -d -p 5005:1234 --name tondocker fabiomp/fabio-brief13
