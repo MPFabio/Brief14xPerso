72940888f496a6de4072046c2da8771d3474b2bb
MPFabio <128690691+MPFabio@users.noreply.github.com>
2023-06-16 09:19:47 +0200

    Update init-script.sh

--- azure-webserver/init-script.sh
+++ azure-webserver/init-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update -y && sudo apt upgrade -y
 #Installing Docker
-sudo apt install docker.io
+sudo curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
 #Creating container        
 sudo docker run -d -p 5005:1234 --name tondocker fabiomp/fabio-brief14
